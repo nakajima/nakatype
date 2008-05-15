f90e68bb214f2340d414fcf37a6c39d011bc4c3d
Pat Nakajima <patnakajima@gmail.com>
2008-05-15 19:30:52 -0400

    added event_hash_changed.js, which uses a periodic updater to fire a custom event whenever the hash changes.

---
+++ src/event_hash_changed.js
@@ -0,0 +1,26 @@
+// Fires a custom event every time the hash changes. It does so by 
+// checking for a change at a set interval (0.2 seconds by default).
+// Also passes the current hash in the memo object to make things
+// easier for event handlers.
+
+(function() {
+  var HashWatcher = {
+    CURRENT: null,
+    INTERVAL: 0.2,
+
+    start: function() {
+      
+      new PeriodicalExecuter(HashWatcher.check, HashWatcher.INTERVAL);
+    },
+
+    check: function(pe) {
+      var hash = window.location.href.split('#')[1];
+      if (HashWatcher.CURRENT != hash) {
+        document.fire('hash:changed', { currentHash: hash });
+        HashWatcher.CURRENT = hash;
+      };
+    }
+  };
+  
+  HashWatcher.start();
+})();
