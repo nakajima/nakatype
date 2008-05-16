435e49a213b41d9815e634ac0d9c8df35afa580a
Pat Nakajima <patnakajima@gmail.com>
2008-05-16 01:19:39 -0400

    updated Benchmark.measure to use firebug's console.time methods. Overwriting the method if not available.

--- src/benchmark.js
+++ src/benchmark.js
@@ -1,14 +1,31 @@
+// Pass in a function, and how many times you want it to be called.
+// Any arguments that the function requires need to be curried in there.
+//
+// NOTE: Only works in Firefox with Firebug enabled.
+//
+// Example:
+//
+//   var addAnElement = function() {
+//     var li = new Element('li');
+//     $('someList').insert(li);
+//   }
+//
+//   Benchmark.measure(addAnElement, 100);
+//
+// TODO: Figure out effect currying has on accuracy.
+// TODO: Allow custom name to be passed in, since "benchmark function" is boring
 var Benchmark = {
   measure: function(fn) {
-    var tests = $A(arguments).slice(1) || 1;
-    var results = [];
-    tests.times(function() {
-      var startTime = new Date().getTime();
-      fn();
-      var endTime = new Date().getTime();
-      var res = endTime - startTime;
-      results.push(res);
-    });
-    console.info('Elapsed time: ' + (results.mean() / 1000) + ' seconds.');
+    var name = 'benchmark function';
+    var tests = $A(arguments).slice(1)[0] || 1;
+    console.time(name)
+    tests.times(fn)
+    console.timeEnd(name)
   }
 };
+
+(function() {
+  if (!(console && console.time && console.timeEnd)) {
+    Benchmark.measure = function() { throw('Firebug not found!'); };
+  };
+})()
