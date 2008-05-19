96458d692b3ce76cbd2f2255073aa01b8db990a7
Pat Nakajima <patnakajima@gmail.com>
2008-05-19 17:12:49 -0400

    added simple shuffle method for arrays

--- src/array_extensions.js
+++ src/array_extensions.js
@@ -7,5 +7,9 @@ Object.extend(Array.prototype, {
   mean: function() {
     var values = this.select(Object.isNumber);
     return values.sum() / values.length;
+  },
+  
+  shuffle: function() {
+    return this.sortBy(Math.random);
   }
 })
