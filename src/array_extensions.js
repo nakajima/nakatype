fd9105589479492bb57746882195f522067522e6
Pat Nakajima <patnakajima@gmail.com>
2008-05-23 15:57:06 -0400

    moved toSentence to proper place, added array tests

--- src/array_extensions.js
+++ src/array_extensions.js
@@ -12,5 +12,17 @@ Object.extend(Array.prototype, {
   // TODO This might not be the most efficient algorithm.
   shuffle: function() {
     return this.sortBy(Math.random);
+  },
+
+  toSentence: function() {
+    var copy = $A(this);
+    var args = $A(arguments);
+    var options = args[0] || { };
+    options.delimiter = options.delimiter || ', ';
+    options.joiner = options.joiner || 'and';
+    if (copy.length < 2) { return copy; }
+
+    var _ending = copy.pop();
+    return copy.join(options.delimiter) + ' ' + options.joiner + ' ' + _ending;
   }
 });
