8282a0cf533f0c21d62ee27fed9c5a9db0fcc9b9
Pat Nakajima <patnakajima@gmail.com>
2008-05-27 11:12:22 -0400

    pulled option setup into its own method, fixed example page

--- src/timer.js
+++ src/timer.js
@@ -1,11 +1,21 @@
+// Simple Timer class for creating timers on your pages. Don't expect it to
+// keep perfect time, due to the problems with JavaScript timers (see John Resig's
+// excellent post on the subject: http://ejohn.org/blog/how-javascript-timers-work/)
+// 
+// You get afterStart and afterPause callbacks, which can be passed in as options
+// when initializing a new Timer object.
 var Timer = Class.create({
   initialize: function(seconds, handler) {
     this.seconds = seconds;
     this.handler = handler;
-    this.options = $A(arguments)[2];
+    this.setupOptions(arguments);    
+    this.start();
+  },
+  
+  setupOptions: function(args) {
+    this.options = $A(args)[2] || { };
     this.options.afterStart = this.options.afterStart || Prototype.emptyFunction;
     this.options.afterPause = this.options.afterPause || Prototype.emptyFunction;
-    this.start();
   },
   
   perform: function() {
